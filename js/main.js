b6fd9095bed605ae8628c683d684f7a92536cc12
DorukSega Yılmaz <dorukyogscast@gmail.com>
2021-03-10 23:17:13 +0300

    initial

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function load(){
+    var HP1SLOT0 =document.getElementById("HP1SLOT0")
+    var option = document.createElement("option"); 
+    option.innerText="test"; 
+    HP1SLOT0.appendChild(option);
+}
