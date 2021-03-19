5493f137a8da44da5174123cb532ce392b6d378a
DorukSega Yılmaz <dorukyogscast@gmail.com>
2021-03-19 18:50:16 +0300

    streamlined

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 function load(){
-    var HP1SLOT0 =document.getElementById("HP1SLOT0")
-    var option = document.createElement("option"); 
-    option.innerText="test"; 
-    HP1SLOT0.appendChild(option);
+    var handcardslist = handcards.split(/\n/gi);
+    //console.log(handcardslist)
+    handcardslist.forEach(element => {
+        var option = document.createElement("option");
+        option.innerText = element;
+        $( ".handcards" ).append(option);
+       
+    });
+    var combatcardslist = combatcards.split(/\n/gi);
+    //console.log(handcardslist)
+    combatcardslist.forEach(element => {
+        var option = document.createElement("option");
+        option.innerText = element;
+        $( ".combatcards" ).append(option);
+       
+    });
+    var improvcardslist = improvcards.split(/\n/gi);
+    //console.log(improvcardslist)
+    improvcardslist.forEach(element => {
+        var option = document.createElement("option");
+        option.innerText = element;
+        $( ".improvcards" ).append(option);
+       
+    });
 }
